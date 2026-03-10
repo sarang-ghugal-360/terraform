9d0efc540c7fef443407a2d0e1da8b61efe393c1
sarang_ghugal_360 <sarangghugal2@gmail.com>
2026-03-10 14:43:30 +0530

    2day

---
+++ Day-2-Terraform-all-config-files/variabls.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+  
+  description = "passing_value to ami_id"
+  default = ""
+  type = string
+}
+variable "instance_type" {
+  description = "passing value to ami_id"
+  default = ""
+  type = string
+}
