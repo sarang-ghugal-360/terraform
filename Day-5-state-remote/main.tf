89142161ef4f9a088dda8ef628c5867d8442b4b2
sarang_ghugal_360 <sarangghugal2@gmail.com>
2026-03-12 16:15:18 +0530

    done

---
+++ Day-5-state-remote/main.tf
@@ -0,0 +1,13 @@
+resource "aws_vpc" "name" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "sarang2"
+  }
+}
+resource "aws_instance" "name" {
+   ami = "ami-02dfbd4ff395f2a1b"
+   instance_type = "t2.micro"
+   tags = {
+     Name ="sarang"
+   }
+}
